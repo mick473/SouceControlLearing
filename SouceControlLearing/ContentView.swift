eb8b09316d7c965d4f4e0f05ef5f92d11d93202c
mick473 <saber-zero@hotmail.com>
2021-04-12 00:39:05 +0800

    Initial Commit

---
+++ SouceControlLearing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SouceControlLearing
+//
+//  Created by ab on 12/4/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
